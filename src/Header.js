347a5f53019d689300601a01972a852c78ca69cb
Andei Khabarov <akhabarow@gmail.com>
2020-02-09 20:37:51 +0300

    Styleguide fixes

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Header = () => {
-    return (
-      <h1>Hello, i am HEADER</h1>
-    )
+	return (
+		<h1>Hello, i am HEADER</h1>
+	)
 }
 
 export default Header;
--- src/Technologies.js
+++ src/Technologies.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Technologies = () => {
-    return(
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-      </ul>
-    );  
+	return (
+		<ul>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+		</ul>
+	);
 }
 
 export default Technologies;
