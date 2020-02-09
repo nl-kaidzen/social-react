1ab29197c3b768986fe02811dc2d95d998992c08
Andei Khabarov <akhabarow@gmail.com>
2020-02-09 20:36:44 +0300

    First 10 laessons

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Technologies = () => {
+    return(
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    );  
+}
+
+export default Technologies;
